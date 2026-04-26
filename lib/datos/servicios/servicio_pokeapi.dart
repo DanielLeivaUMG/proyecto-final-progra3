4722e145e1006d1b5a3401f388820f9d71b8dc81
HetzonL27 <hagustine@miumg.edu.gt>
2026-04-26 13:49:10 -0600

    [INT2][E1] agregar consumo inicial de lista pokemon con http

--- lib/datos/servicios/servicio_pokeapi.dart
+++ lib/datos/servicios/servicio_pokeapi.dart
@@ -1,7 +1,27 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:proyecto_final_progra3/datos/modelos/modelo_pokemon.dart';
 import 'package:proyecto_final_progra3/nucleo/constantes/api_constantes.dart';
 
 class ServicioPokeapi {
   String obtenerUrlPokemon() {
     return '${ApiConstantes.urlBase}${ApiConstantes.endpointPokemon}';
+  }
+
+  Future<List<Pokemon>> obtenerPokemones() async {
+    final Uri url = Uri.parse('${obtenerUrlPokemon()}?limit=20&offset=0');
+    final http.Response respuesta = await http.get(url);
+
+    if (respuesta.statusCode == 200) {
+      final Map<String, dynamic> datos = json.decode(respuesta.body);
+      final List<dynamic> resultados = datos['results'] ?? <dynamic>[];
+
+      return resultados
+          .map((dynamic item) => Pokemon.fromJson(item as Map<String, dynamic>))
+          .toList();
+    } else {
+      throw Exception('Error al obtener los pokemones desde la API');
+    }
   }
 }
